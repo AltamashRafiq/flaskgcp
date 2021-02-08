17a26b39c9a9ab54d860172f56c6dec8ebe4343c
Altamash Rafiq <altamashrafiq@gmail.com>
2021-02-08 19:23:25 +0000

    fixed continuous delivery issue caused by service accounts settings

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World, this is Altamash!' # small change
+    return 'Hello World, this is Altamash!'
 
 @app.route('/name/<value>')
 def name(value):
